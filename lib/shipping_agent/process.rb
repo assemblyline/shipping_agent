a00c359c5d31e94b52eaf63b2c52feb8d41bd567
Ed Robinson <ed.robinson@reevoo.com>
2015-03-02 13:27:12 +0000

    Add process spec

--- lib/shipping_agent/process.rb
+++ lib/shipping_agent/process.rb
@@ -1,4 +1,10 @@
 module ShippingAgent
   class Process
+    def initialize(name:, command:)
+      @name = name
+      @command = command
+    end
+
+    attr_reader :name, :command
   end
 end
--- spec/unit/process_spec.rb
+++ spec/unit/process_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require 'shipping_agent/process'
 
 describe ShippingAgent::Process do
-  subject { described_class.new(name
+  subject { described_class.new(name: 'foo', command: 'bundle exec rake foo') }
+
+  it 'has a name and command' do
+    expect(subject.name).to eq 'foo'
+    expect(subject.command).to eq 'bundle exec rake foo'
+  end
 end
