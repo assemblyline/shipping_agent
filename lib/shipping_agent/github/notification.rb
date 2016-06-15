087dc5f94132399e0031ac59f462162bb1dd5b8b
Ed Robinson <ed@reevoo.com>
2016-06-15 14:46:05 +0100

    When the deployment is successful mark old deployments as inactive
    When the deployment is successful we should mark all the older
    deployments in the same environment as inactive.

--- lib/shipping_agent/github/notification.rb
+++ lib/shipping_agent/github/notification.rb
@@ -12,6 +12,7 @@ module ShippingAgent
           accept: "application/vnd.github.ant-man-preview+json",
           description: description,
         )
+        mark_old_deployments_inactive(deploy) if status == "success"
       rescue => e
         LOGGER.warn do
           "Failed to update github with: [#{status}] #{description} - due to: #{e.class} #{e.message}"
@@ -19,6 +20,34 @@ module ShippingAgent
       end
 
       private
+
+      # Github has an option to do this automaticly, but it only works
+      # for non-production environments.
+      def mark_old_deployments_inactive(deploy)
+        active_old_deployments(deploy).each { |deployment| mark_inactive(deployment) }
+      end
+
+      def active_old_deployments(current)
+        old_deployments(current).reject do |deployment|
+          current.url == deployment.url ||
+            deployment.rels[:statuses].get.data.any? { |s| s.state == "inactive" }
+        end
+      end
+
+      def old_deployments(deploy)
+        github.get(
+          github.get(deploy.url).rels[:repository].get.data.deployments_url,
+          environment: deploy.namespace,
+        )
+      end
+
+      def mark_inactive(deployment)
+        github.create_deployment_status(
+          deployment.url,
+          "inactive",
+          accept: "application/vnd.github.ant-man-preview+json",
+        )
+      end
 
       def github
         @github_client ||= Octokit::Client.new(access_token: ENV.fetch("GITHUB_TOKEN"))
